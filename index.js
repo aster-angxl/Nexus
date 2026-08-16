2094441cc4c9725270f025003951cbf22348125d
aster-angxl <system.cloudy123@gmail.com>
2026-08-16 11:21:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-const { Client, GatewayIntentBits } require de discord.js
+require('dotenv').config();
+
+const {
+  Client,
+  GatewayIntentBits,
+} = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`Nexus est connecté en tant que ${client.user.tag}`);
+});
+
+client.on('error', (error) => {
+  console.error('Erreur Discord :', error);
+});
+
+client.login(process.env.DISCORD_TOKEN);
 
